ef85e6239d051b036c57ea3fe1a50299c41ce1cb
Danijel Predojevic <predojevic.danijel@gmail.com>
2015-08-21 18:48:29 +0200

    Start working on edit image

--- js/admin.js
+++ js/admin.js
@@ -48,6 +48,7 @@
 		      button: {
 		        text: 'Use This One'
 		      },
+		      state:    'insert',
 		      library : { type : 'image' },
 		      multiple: false  // Set to true to allow multiple files to be selected
 		    });
@@ -68,6 +69,61 @@
 
 		      // Remove image button
 		      $this.remove();
+		      //console.log(frame.state().get);
+		    });
+
+		});
+
+		$("body").on("click",".swifty_img_holder .swifty_edit_img", function(e) {
+			
+			var $this = $(this);
+			imgContainer = $this.prev(".imgContainer");
+			imgIdInput = $this.parent().find('.imgIdInput');
+			e.preventDefault();
+			
+		    if ( frame ) {
+		      frame.open();
+		      return;
+		    }
+
+		    // Create a new media frame
+		    frame = wp.media({
+		      title: 'Select Image',
+		      button: {
+		        text: 'Use This One'
+		      },
+		      state:    'insert',
+		      library : { type : 'image' },
+		      multiple: false, // Set to true to allow multiple files to be selected
+		    });
+
+		    frame.open();
+		    console.log(frame);
+
+		    // When an image is selected in the media frame...
+		    frame.on( 'select', function() {
+		      
+		      // Get media attachment details from the frame state
+		      var attachment = frame.state().get('selection').first().toJSON();
+
+		      // Send the attachment URL to our custom image input field.
+		      imgContainer.append( '<img src="'+attachment.url+'" alt="" style="max-width:100%;"/>' );
+
+		      // Send the attachment id to our hidden input
+		      imgIdInput.val( attachment.id );
+
+		      // Remove image button
+		      $this.remove();
+		      //console.log(frame.state().get);
+		    });
+
+		    // When an image is selected in the media frame...
+		    frame.on( 'open', function() {
+		    	console.log("yeah");
+		      	var selection = frame.state().get('selection');
+				attachment = wp.media.attachment(imgIdInput.val());
+				attachment.fetch();
+				selection.add( attachment ? [ attachment ] : [] );
 		    });
 
 		});
